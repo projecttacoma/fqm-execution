2d884dee2332769152f5397d86601a77baefc514
Michael O'Keefe <mokeefe@mitre.org>
2020-10-02 13:55:07 -0400

    add functions to DebugHelper

--- src/DebugHelper.ts
+++ src/DebugHelper.ts
@@ -2,26 +2,55 @@ import { ELM } from './types/ELMTypes';
 import { ValueSetMap } from 'cql-execution';
 import fs = require('fs');
 
-export function dumpELMJSONs(elmJSONs:ELM[]):void {
+export function dumpELMJSONs(elmJSONs: ELM[]): void {
+  // create folder if it doesn't exist
+  if (!fs.existsSync('debug/elm')) {
+    fs.mkdirSync('debug/elm', { recursive: true });
+  }
+
+  // wipe out anything from previous run
   const files = fs.readdirSync('debug/elm');
   if (files) {
     files.forEach(file => {
       fs.unlinkSync(`debug/elm/${file}`);
     });
   }
-  fs.mkdirSync('debug/elm', { recursive: true });
+
   elmJSONs.forEach(elmJSON => {
-    fs.writeFileSync(`debug/elm/${elmJSON.library.identifier.id}.json`, JSON.stringify(elmJSON, null, 2));
+    fs.writeFileSync(
+      `debug/elm/${elmJSON.library.identifier.id}-${elmJSON.library.identifier.version}.json`,
+      JSON.stringify(elmJSON, null, 2)
+    );
   });
 }
 
 export function dumpVSMap(VSMap: ValueSetMap): void {
+  // create folder if it doesn't exist
+  if (!fs.existsSync('debug/vs')) {
+    fs.mkdirSync('debug/vs', { recursive: true });
+  }
+
+  // wipe out anything from previous run
   const files = fs.readdirSync('debug/vs');
   if (files) {
     files.forEach(file => {
       fs.unlinkSync(`debug/vs/${file}`);
     });
   }
-  fs.mkdirSync('debug/vs', { recursive: true });
+
   fs.writeFileSync('debug/vs/vsmap.json', JSON.stringify(VSMap, null, 2));
+}
+
+export function dumpObject(object: any, nameInDebugFolder: string): void {
+  // create debug folder if it doesnt exist
+  if (!fs.existsSync('debug')) {
+    fs.mkdirSync('debug', { recursive: true });
+  }
+
+  // delete old copy
+  if (fs.existsSync(`debug/${nameInDebugFolder}`)) {
+    fs.unlinkSync(`debug/${nameInDebugFolder}`);
+  }
+
+  fs.writeFileSync(`debug/${nameInDebugFolder}`, JSON.stringify(object, null, 2));
 }
