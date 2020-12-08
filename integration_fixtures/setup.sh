be2c0ddcdc12322f00e3f862ca332e21a0dd2505
mriley <mriley@mitre.org>
2020-12-08 14:28:54 -0500

    Update setup.sh

--- integration_fixtures/setup.sh
+++ integration_fixtures/setup.sh
@@ -6,9 +6,13 @@ git clone --single-branch --branch master https://github.com/projecttacoma/fhir-
 echo "> Fetching CQF-tooling JAR"
 ./travis/download_cqf_tooling.sh
 
+echo "> Cloning fqm execution service..."
+git clone --single-branch --branch master https://github.com/projecttacoma/fqm-execution-service.git
+
 echo "> Setting Up fqm execution service"
 #docker pull contentgroup/cqf-ruler:develop
 #docker run --name cqf-ruler --rm -dit -p 8080:8080 contentgroup/cqf-ruler:develop
+npm start 
 
 echo "> Waiting for fqm execution service to start up"
 until `curl --output /dev/null --silent --head --fail http://localhost:3000/POST/calculateRaw`; do printf '.'; sleep 5; done
