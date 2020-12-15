530cf4e8c456191fd8629b132f1d49fece00baab
mriley <mriley@mitre.org>
2020-12-15 12:59:56 -0500

    moved files

--- .github/test_measures.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "> Rebuilding Measure Bundles and Loading CQF-Ruler"
-if ! ./integration_fixtures/rebuild_bundles.sh; then
-  exit 1
-fi
-
-echo "> Starting Calculation and Comparison of MeasureReports"
-node ./integration_fixtures/calcurun_tests.ts $1
