c216256c6368a3a8f91a660113a1f03ddcfacb59
mriley <mriley@mitre.org>
2020-12-15 12:59:56 -0500

    moved files

--- _tests_/setup.sh
+++ _tests_/setup.sh
@@ -3,6 +3,4 @@ echo "> Cloning FHIR Generated Patients..."
 git clone --single-branch --branch master https://github.com/projecttacoma/fhir-patient-generator.git
 
 echo "> Cloning connectathon repo"
-git clone --single-branch --branch master https://github.com/projecttacoma/connectathon.git  
-
-
+git clone --single-branch --branch master https://github.com/projecttacoma/connectathon.git
