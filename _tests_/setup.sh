90850b3db51ae9dc274db0d5d1ad8acbed14cacd
mriley <mriley@mitre.org>
2021-01-06 12:23:24 -0500

    address pr comments

--- _tests_/setup.sh
+++ _tests_/setup.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
 
 echo "> Cloning FHIR Generated Patients..."
 git clone --single-branch --branch master https://github.com/projecttacoma/fhir-patient-generator.git
 
+echo "> Cloning connectathon repo"
 git clone --single-branch --branch master https://github.com/projecttacoma/connectathon.git  
+
+echo "> running the tests"
+node ./run_tests.ts
 
