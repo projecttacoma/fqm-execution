fa0555b149304f3479f30a73b55c57549c808e88
mriley <mriley@mitre.org>
2021-01-06 11:46:21 -0500

    address pr review feedback

--- _tests_/test_measures.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "> Rebuilding Measure Bundles and Loading CQF-Ruler"
-if ! ./_tests_/rebuild_bundles.sh; then
-  exit 1
-fi
-
-echo "> Starting Calculation and Comparison of MeasureReports"
-node ./_tests_/run_tests.ts $1
