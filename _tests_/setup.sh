ae4cb08be1796f30a0372d1f06a3167b20c6f25f
MegRiley <53576881+MegRiley@users.noreply.github.com>
2021-01-12 10:39:34 -0500

    Integration test fixtures (#25)
    * initial changes
    
    * Update setup.sh
    
    * Create run_tests.ts
    
    * minor changes to not require fqm execution service
    
    * swapping from js to ts
    
    * using the folders added when moving to actions
    
    * moved files
    
    * changes to reflect the correct pathing required by github actions
    
    * Update ci.yml
    
    * first round of lint fixes
    
    * linting changs
    
    * Update fhirInteractions.ts
    
    * Address import issues
    
    * Update ci.yml
    
    * initial changes
    
    * Update setup.sh
    
    * Create run_tests.ts
    
    * minor changes to not require fqm execution service
    
    * swapping from js to ts
    
    * using the folders added when moving to actions
    
    * moved files
    
    * changes to reflect the correct pathing required by github actions
    
    * Update ci.yml
    
    * first round of lint fixes
    
    * linting changs
    
    * Update ci.yml
    
    * minor updates
    
    * Update ci.yml
    
    * address pr feedbacl
    
    * address pr review feedback
    
    * address pr comments
    
    * removed files that are not part of the fixtures
    
    * Address PR feedback
    
    * Update setup.sh
    
    * Update .github/workflows/ci.yml
    
    Co-authored-by: hossenlopp <hossenlopp@mitre.org>
    
    * Update setup.sh
    
    Co-authored-by: hossenlopp <hossenlopp@mitre.org>

---
+++ _tests_/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "> Cloning FHIR Generated Patients..."
+git clone --single-branch --branch master https://github.com/projecttacoma/fhir-patient-generator.git
+
+echo "> Cloning connectathon repo"
+git clone --single-branch --branch master https://github.com/projecttacoma/connectathon.git  
+
+
