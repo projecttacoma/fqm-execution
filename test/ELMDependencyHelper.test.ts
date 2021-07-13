028c337e4ac3f9b1e6110e3469cd6ffd8b47c6e1
Matthew Gramigna <mgramigna@mitre.org>
2021-07-13 11:04:57 -0400

    Add tests for new library lookup functions

--- test/ELMDependencyHelper.test.ts
+++ test/ELMDependencyHelper.test.ts
@@ -1,11 +1,16 @@
 import * as ELMDependencyHelper from '../src/ELMDependencyHelper';
 import { getELMFixture } from './helpers/testHelpers';
+import { ELM } from '../src/types/ELMTypes';
 
 describe('ELMDependencyHelper', () => {
+  let elm: ELM;
+  beforeAll(() => {
+    elm = getELMFixture('elm/EXM130/EXM130.json');
+  });
+
   describe('buildStatementDependencyMaps', () => {
     test('can create dependency map for statements', () => {
-      const elms = [getELMFixture('elm/EXM130/EXM130.json')];
-      const libInfos = ELMDependencyHelper.buildStatementDependencyMaps(elms);
+      const libInfos = ELMDependencyHelper.buildStatementDependencyMaps([elm]);
 
       // check top level
       expect(libInfos.length).toEqual(1);
@@ -25,6 +30,46 @@ describe('ELMDependencyHelper', () => {
           { libraryId: 'MATGlobalCommonFunctions', statementName: 'Normalize Interval' }
         ]);
       }
+    });
+  });
+
+  describe('findLibraryReferenceId', () => {
+    let elm: ELM;
+    beforeAll(() => {
+      elm = getELMFixture('elm/EXM130/EXM130.json');
+    });
+    test('should find matching include by localIdentifier', () => {
+      const libraryPath = ELMDependencyHelper.findLibraryReferenceId(elm, 'Global');
+
+      expect(libraryPath).toEqual('MATGlobalCommonFunctions');
+    });
+
+    test('should return null on missing include', () => {
+      const libraryPath = ELMDependencyHelper.findLibraryReferenceId(elm, 'does-not-exist');
+
+      expect(libraryPath).toBeNull();
+    });
+  });
+
+  describe('findLibraryReference', () => {
+    let elm: ELM;
+    let dependency: ELM;
+    beforeAll(() => {
+      elm = getELMFixture('elm/queries/SimpleQueries.json');
+      dependency = getELMFixture('elm/queries/SimpleQueriesDependency.json');
+    });
+
+    test('should find matching library by localIdentifier', () => {
+      const matchingLib = ELMDependencyHelper.findLibraryReference(elm, [elm, dependency], 'SimpleDep');
+
+      expect(matchingLib).not.toBeNull();
+      expect(matchingLib?.library.identifier.id).toEqual(dependency.library.identifier.id);
+    });
+
+    test('should return null on missing include', () => {
+      const matchingLib = ELMDependencyHelper.findLibraryReference(elm, [elm, dependency], 'does-not-exist');
+
+      expect(matchingLib).toBeNull();
     });
   });
 });
