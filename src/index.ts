70834ccc9353e85c4b69707e5b6738884a814fb8
Michael O'Keefe <mokeefe@mitre.org>
2021-08-04 16:27:06 -0400

    Export required helpers in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * as Calculator from './calculation/Calculator';
+export * as ELMHelpers from './helpers/elm/ELMHelpers';
+export * as ELMDependencyHelpers from './helpers/elm/ELMDependencyHelpers';
+export { extractMeasureFromBundle, extractLibrariesFromBundle } from './calculation/ClauseResultsHelpers';
 export * from './types';
