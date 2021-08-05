500eab0d2b1086e1062f706a4a4ec72ad1a64827
Michael O'Keefe <mokeefe@mitre.org>
2021-08-05 10:14:25 -0400

    Migrate Bundle-related helpers to their own helper

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * as Calculator from './calculation/Calculator';
 export * as ELMHelpers from './helpers/elm/ELMHelpers';
 export * as ELMDependencyHelpers from './helpers/elm/ELMDependencyHelpers';
-export { extractMeasureFromBundle, extractLibrariesFromBundle } from './calculation/ClauseResultsHelpers';
+export * as MeasureBundleHelpers from './helpers/MeasureBundleHelpers';
 export * from './types';
