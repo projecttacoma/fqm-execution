b60555b1deaa9b4c736cd9d2ef8c225c8cab2acb
Matthew Gramigna <mgramigna@mitre.org>
2021-08-10 08:34:37 -0400

    Export find retrieves (#86)
    * Add additional export for findRetrieves
    
    * 0.6.1

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * as Calculator from './calculation/Calculator';
 export * as ELMHelpers from './helpers/elm/ELMHelpers';
 export * as ELMDependencyHelpers from './helpers/elm/ELMDependencyHelpers';
 export * as MeasureBundleHelpers from './helpers/MeasureBundleHelpers';
+export * as RetrievesFinder from './gaps/RetrievesFinder';
 export * from './types';
