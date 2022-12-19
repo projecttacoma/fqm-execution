dca3ab90c0be47d4e6175f60d2eeb4d4f64f3c1f
natjoe4 <njjones@mitre.org>
2022-12-19 14:30:56 -0500

    matt's comments

--- test/integration/helpers/testHelpers.ts
+++ test/integration/helpers/testHelpers.ts
@@ -1,8 +1,36 @@
 import { readFileSync } from 'fs';
+import { DetailedPopulationGroupResult, ExecutionResult, PopulationResult } from '../../../src/types/Calculator';
+import { PopulationType } from '../../../src/types/Enums';
 
 /**
  * Retrieves JSON fixture from the integration directory for easier ts casting in test files
  */
 export function getJSONFixture(path: string): any {
   return JSON.parse(readFileSync(`test/integration/${path}`).toString());
+}
+
+/**
+ * Formats expectedPopulations object into an array of objects which can be passed into an
+ * expect.arrayContaining and compared with result
+ */
+export function assertPopulationResults(
+  result: ExecutionResult<DetailedPopulationGroupResult>,
+  expectedPopulations: Partial<Record<PopulationType, boolean>>
+) {
+  const expectedPopulationResults: PopulationResult[] = Object.entries(expectedPopulations).map(([popType, value]) =>
+    expect.objectContaining({
+      populationType: popType as PopulationType,
+      result: value
+    })
+  );
+
+  expect(result).toEqual(
+    expect.objectContaining({
+      detailedResults: expect.arrayContaining([
+        expect.objectContaining({
+          populationResults: expectedPopulationResults
+        })
+      ])
+    })
+  );
 }
--- test/integration/proportion-boolean/proportion-boolean.test.ts
+++ test/integration/proportion-boolean/proportion-boolean.test.ts
@@ -1,6 +1,6 @@
 import { calculate } from '../../../src/calculation/Calculator';
 import { CalculationOptions } from '../../../src/types/Calculator';
-import { getJSONFixture } from '../helpers/testHelpers';
+import { assertPopulationResults, getJSONFixture } from '../helpers/testHelpers';
 
 const DEFAULT_CALCULATION_OPTIONS: CalculationOptions = {
   measurementPeriodStart: '2022-01-01',
@@ -20,42 +20,21 @@ const MEASURE_BUNDLE: fhir4.Bundle = getJSONFixture('proportion-boolean/proporti
 describe('proportion boolean measure', () => {
   it('calculates ipp patient into ipp', async () => {
     const results = await calculate(MEASURE_BUNDLE, [IPP_PATIENT_BUNDLE], DEFAULT_CALCULATION_OPTIONS);
-    expect(results.results[0].detailedResults?.[0]?.populationResults).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({ populationType: 'initial-population', result: true }),
-        expect.objectContaining({ populationType: 'denominator', result: false }),
-        expect.objectContaining({ populationType: 'numerator', result: false })
-      ])
-    );
+    assertPopulationResults(results.results[0], { 'initial-population': true, denominator: false, numerator: false });
   });
+
   it('calculates denom patient into denom', async () => {
     const results = await calculate(MEASURE_BUNDLE, [DENOM_PATIENT_BUNDLE], DEFAULT_CALCULATION_OPTIONS);
-    expect(results.results[0].detailedResults?.[0]?.populationResults).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({ populationType: 'initial-population', result: true }),
-        expect.objectContaining({ populationType: 'denominator', result: true }),
-        expect.objectContaining({ populationType: 'numerator', result: false })
-      ])
-    );
+    assertPopulationResults(results.results[0], { 'initial-population': true, denominator: true, numerator: false });
   });
+
   it('calculates numer patient into numer', async () => {
     const results = await calculate(MEASURE_BUNDLE, [NUMER_PATIENT_BUNDLE], DEFAULT_CALCULATION_OPTIONS);
-    expect(results.results[0].detailedResults?.[0]?.populationResults).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({ populationType: 'initial-population', result: true }),
-        expect.objectContaining({ populationType: 'denominator', result: true }),
-        expect.objectContaining({ populationType: 'numerator', result: true })
-      ])
-    );
+    assertPopulationResults(results.results[0], { 'initial-population': true, denominator: true, numerator: true });
   });
+
   it('calculates numer into denom for different measurement period', async () => {
     const results = await calculate(MEASURE_BUNDLE, [IPP_PATIENT_BUNDLE], EARLIER_PERIOD_CALCULATION_OPTIONS);
-    expect(results.results[0].detailedResults?.[0]?.populationResults).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({ populationType: 'initial-population', result: false }),
-        expect.objectContaining({ populationType: 'denominator', result: false }),
-        expect.objectContaining({ populationType: 'numerator', result: false })
-      ])
-    );
+    assertPopulationResults(results.results[0], { 'initial-population': false, denominator: false, numerator: false });
   });
 });
