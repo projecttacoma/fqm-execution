d414e8820cd447f63286995bcc7c56db5893319b
Chris Hossenlopp <hossenlopp@mitre.org>
2022-12-21 18:10:11 -0500

    fixed issue with bundle building causing execution error

--- test/integration/ratio-Encounter-reuseObservationFunction/ratio-Encounter-reuseObservationFunction.test.ts
+++ test/integration/ratio-Encounter-reuseObservationFunction/ratio-Encounter-reuseObservationFunction.test.ts
@@ -10,8 +10,9 @@ export function getJSONFixture(path: string): any {
 }
 
 const CALCULATION_OPTIONS: CalculationOptions = {
-  measurementPeriodStart: '2022-01-01',
-  measurementPeriodEnd: '2022-12-31'
+  measurementPeriodStart: '2023-01-01',
+  measurementPeriodEnd: '2023-12-31',
+  enableDebugOutput: true
 };
 
 const MEASURE_BUNDLE: fhir4.Bundle = getJSONFixture(
@@ -25,6 +26,9 @@ const PATIENT_BUNDLE: fhir4.Bundle = getJSONFixture(
 describe('ratio Encounter reuse observation function measure', () => {
   it('calculates ipp patient into ipp', async () => {
     const results = await calculate(MEASURE_BUNDLE, [PATIENT_BUNDLE], CALCULATION_OPTIONS);
-    console.log(results.results[0].detailedResults);
+    if (results.results[0].detailedResults) {
+      console.log(results.results[0].detailedResults[0].populationResults);
+    }
+    debugger;
   });
 });
