4a514f6d9e40c87177a6783ac05cb764dc8d765f
Chris Hossenlopp <hossenlopp@mitre.org>
2022-12-28 18:16:51 -0500

    created separate test for episode results and improved results

--- test/integration/ratio-Encounter-reuseObservationFunction/ratio-Encounter-reuseObservationFunction.test.ts
+++ test/integration/ratio-Encounter-reuseObservationFunction/ratio-Encounter-reuseObservationFunction.test.ts
@@ -18,7 +18,7 @@ const PATIENT_BUNDLE: fhir4.Bundle = getJSONFixture(
 );
 
 describe('ratio Encounter reuse observation function measure', () => {
-  it('does not put all results on observation usage of a function on one observation', async () => {
+  it('does not put all results on observation usage of a function on one observation in population results', async () => {
     const results = await calculate(MEASURE_BUNDLE, [PATIENT_BUNDLE], CALCULATION_OPTIONS);
     if (!results.results[0].detailedResults) {
       fail('Population DetailedResults not found');
@@ -31,13 +31,42 @@ describe('ratio Encounter reuse observation function measure', () => {
     }
     expect(numerResults.populationId).toBeDefined();
     expect(denomResults.populationId).toBeDefined();
-    const numerObervResults = popResults.find(
+    const numerObservResults = popResults.find(
       r => r.populationType === PopulationType.OBSERV && r.criteriaReferenceId === numerResults.populationId
     );
-    expect(numerObervResults?.observations).toEqual([1]);
-    const denomObervResults = popResults.find(
+    const denomObservResults = popResults.find(
       r => r.populationType === PopulationType.OBSERV && r.criteriaReferenceId === denomResults.populationId
     );
+    expect(numerObservResults).toBeDefined();
+    expect(denomObservResults).toBeDefined();
+    expect(numerObservResults?.observations).toEqual([1]);
+    expect(denomObservResults?.observations).toEqual([1]);
+  });
+
+  it('does not put all results on observation usage of a function on one observation in episode results', async () => {
+    const results = await calculate(MEASURE_BUNDLE, [PATIENT_BUNDLE], CALCULATION_OPTIONS);
+    if (!results.results[0].detailedResults) {
+      fail('Population DetailedResults not found');
+    }
+    const episodeResults = results.results[0].detailedResults[0].episodeResults?.find(r => r.episodeId === '3');
+    if (!episodeResults) {
+      fail('Could not find results for Encounter episode with id "3"');
+    }
+
+    const numerResults = episodeResults.populationResults.find(r => r.populationType === PopulationType.NUMER);
+    const denomResults = episodeResults.populationResults.find(r => r.populationType === PopulationType.DENOM);
+    if (!numerResults || !denomResults) {
+      fail('NUMER or DENOM results not found');
+    }
+    expect(numerResults.populationId).toBeDefined();
+    expect(denomResults.populationId).toBeDefined();
+    const numerObervResults = episodeResults.populationResults.find(
+      r => r.populationType === PopulationType.OBSERV && r.criteriaReferenceId === numerResults.populationId
+    );
+    const denomObervResults = episodeResults.populationResults.find(
+      r => r.populationType === PopulationType.OBSERV && r.criteriaReferenceId === denomResults.populationId
+    );
+    expect(numerObervResults?.observations).toEqual([1]);
     expect(denomObervResults?.observations).toEqual([1]);
   });
 });
