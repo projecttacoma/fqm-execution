d02b0913cea524ea78b57603fc962ce557a3d073
Chris Hossenlopp <hossenlopp@mitre.org>
2023-01-29 18:45:25 -0500

    added more integration tests for the ratio measure situations

--- test/integration/ratio-Encounter-reuseObservationFunction/ratio-Encounter-reuseObservationFunction.test.ts
+++ test/integration/ratio-Encounter-reuseObservationFunction/ratio-Encounter-reuseObservationFunction.test.ts
@@ -30,6 +30,18 @@ const PATIENT_2ENC_1IN_DENOM_ONLY: fhir4.Bundle = getJSONFixture(
   'ratio-Encounter-reuseObservationFunction/patients/patient-2enc-1in-denom-only.json'
 );
 
+const PATIENT_2ENC_2IN_DENOM_1IN_NUMER: fhir4.Bundle = getJSONFixture(
+  'ratio-Encounter-reuseObservationFunction/patients/patient-2enc-2in-denom-1in-numer.json'
+);
+
+const PATIENT_2ENC_1IN_DENOM_1IN_NUMER: fhir4.Bundle = getJSONFixture(
+  'ratio-Encounter-reuseObservationFunction/patients/patient-2enc-1in-denom-1in-numer.json'
+);
+
+const PATIENT_3ENC_1IN_ALL_2IN_DENOM: fhir4.Bundle = getJSONFixture(
+  'ratio-Encounter-reuseObservationFunction/patients/patient-3enc-1in-all-2in-denom.json'
+);
+
 describe('ratio Encounter reuse observation function measure', () => {
   describe('encounter in NUMER and DENOM', () => {
     it('does not put all results on observation usage of a function on one observation in population results', async () => {
@@ -85,6 +97,79 @@ describe('ratio Encounter reuse observation function measure', () => {
 
       assertEpisodeObservations(group, episodeId, [1], PopulationType.DENOM);
       assertEpisodeObservations(group, episodeId, undefined, PopulationType.NUMER);
+    });
+  });
+
+  describe('2 encounter in DENOM, 1 in NUMER', () => {
+    it('allows for both in DENOM and one in NUMER', async () => {
+      const results = await calculate(MEASURE_BUNDLE, [PATIENT_2ENC_2IN_DENOM_1IN_NUMER], CALCULATION_OPTIONS);
+      const group = getGroupByIndex(0, results.results[0]);
+
+      assertGroupObservations(group, [1, 3], PopulationType.DENOM);
+      assertGroupObservations(group, [3], PopulationType.NUMER);
+    });
+
+    it('allows for both in DENOM and one in NUMER in episode results', async () => {
+      const results = await calculate(MEASURE_BUNDLE, [PATIENT_2ENC_2IN_DENOM_1IN_NUMER], CALCULATION_OPTIONS);
+      const group = getGroupByIndex(0, results.results[0]);
+
+      const episodeIdDenomNumer = '1c99e052-c4f1-412b-b0e0-721bb275a091';
+      assertEpisodeObservations(group, episodeIdDenomNumer, [3], PopulationType.DENOM);
+      assertEpisodeObservations(group, episodeIdDenomNumer, [3], PopulationType.NUMER);
+
+      const episodeIdDenomOnly = 'e7f2b9f4-5be0-4a6b-8602-8721949f1885';
+      assertEpisodeObservations(group, episodeIdDenomOnly, [1], PopulationType.DENOM);
+      assertEpisodeObservations(group, episodeIdDenomOnly, undefined, PopulationType.NUMER);
+    });
+  });
+
+  describe('2 encounter, 1 in DENOM, 1 in NUMER', () => {
+    it('allows for one in DENOM and one in NUMER', async () => {
+      const results = await calculate(MEASURE_BUNDLE, [PATIENT_2ENC_1IN_DENOM_1IN_NUMER], CALCULATION_OPTIONS);
+      const group = getGroupByIndex(0, results.results[0]);
+
+      assertGroupObservations(group, [1], PopulationType.DENOM);
+      assertGroupObservations(group, [3], PopulationType.NUMER);
+    });
+
+    it('allows for one in DENOM and one in NUMER in episode results', async () => {
+      const results = await calculate(MEASURE_BUNDLE, [PATIENT_2ENC_1IN_DENOM_1IN_NUMER], CALCULATION_OPTIONS);
+      const group = getGroupByIndex(0, results.results[0]);
+
+      const episodeIdDenomOnly = '99da356c-0344-496a-bd48-bee6f797e175';
+      assertEpisodeObservations(group, episodeIdDenomOnly, [1], PopulationType.DENOM);
+      assertEpisodeObservations(group, episodeIdDenomOnly, undefined, PopulationType.NUMER);
+
+      const episodeIdNumerOnly = 'f9de7f21-953a-4bf1-a17c-599eb0b8f533';
+      assertEpisodeObservations(group, episodeIdNumerOnly, undefined, PopulationType.DENOM);
+      assertEpisodeObservations(group, episodeIdNumerOnly, [3], PopulationType.NUMER);
+    });
+  });
+
+  describe('3 encounter, 1 in DENOM/NUMER, 2 in DENOM', () => {
+    it('allows for one in DENOM and one in NUMER', async () => {
+      const results = await calculate(MEASURE_BUNDLE, [PATIENT_3ENC_1IN_ALL_2IN_DENOM], CALCULATION_OPTIONS);
+      const group = getGroupByIndex(0, results.results[0]);
+
+      assertGroupObservations(group, [1, 3, 2], PopulationType.DENOM);
+      assertGroupObservations(group, [1], PopulationType.NUMER);
+    });
+
+    it('allows for one in DENOM and one in NUMER in episode results', async () => {
+      const results = await calculate(MEASURE_BUNDLE, [PATIENT_3ENC_1IN_ALL_2IN_DENOM], CALCULATION_OPTIONS);
+      const group = getGroupByIndex(0, results.results[0]);
+
+      const episodeIdDenomNumer = '73dd8d81-77d8-46e7-984c-a3db88797d43';
+      assertEpisodeObservations(group, episodeIdDenomNumer, [1], PopulationType.DENOM);
+      assertEpisodeObservations(group, episodeIdDenomNumer, [1], PopulationType.NUMER);
+
+      const episodeIdDenom1 = 'ae5a5635-f2ed-40d4-a1bd-ec93181ae30a';
+      assertEpisodeObservations(group, episodeIdDenom1, [3], PopulationType.DENOM);
+      assertEpisodeObservations(group, episodeIdDenom1, undefined, PopulationType.NUMER);
+
+      const episodeIdDenom2 = '4af3c69d-ada0-4a2e-99d2-dd53b7c0e16c';
+      assertEpisodeObservations(group, episodeIdDenom2, [2], PopulationType.DENOM);
+      assertEpisodeObservations(group, episodeIdDenom2, undefined, PopulationType.NUMER);
     });
   });
 });
