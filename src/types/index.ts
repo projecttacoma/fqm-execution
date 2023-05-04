76853651d650070f031e0e07e2765c9739ad0c14
Matthew Gramigna <mgramigna@mitre.org>
2023-05-04 06:28:54 -0400

    Expose all interfaces from root exports; update docs (#233)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,8 @@
+/**
+ * @deprecated
+ * Prefer importing interfaces directly from root `index` instead of entire type modules with all interfaces
+ * TODO: remove this line for fqm-execution 2.0
+ */
 export * as CalculatorTypes from './Calculator';
 export * as CQLTypes from './CQLTypes';
 export * as ELMTypes from './ELMTypes';
