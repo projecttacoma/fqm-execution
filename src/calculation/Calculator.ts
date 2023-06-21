d2fe9b55c77ebd8bd0dd0806c87bbc0e16447cc0
sarahmcdougall <smcdougall@mitre.org>
2023-06-21 08:59:44 -0400

    add line

--- src/calculation/Calculator.ts
+++ src/calculation/Calculator.ts
@@ -248,6 +248,7 @@ export async function calculate<T extends CalculationOptions>(
     });
 
     patientSource = resolvePatientSource(patientBundles, options);
+
     if (!isCompositeExecution && options.calculateClauseCoverage) {
       groupClauseCoverageHTML = generateClauseCoverageHTML(
         measure,
