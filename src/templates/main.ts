faeac4ce1f927022c8c4a5666f9ada960eb050be
Elsa <eperelli@mitre.org>
2024-04-02 12:19:49 -0400

    Fix unit tests

--- src/templates/main.ts
+++ src/templates/main.ts
@@ -1,4 +1,4 @@
-export default `<pre style="tab-size: 2; {{~#if @root.highlightLogic~}} line-height: 1.51em{{~/if~}}"
+export default `<pre style="tab-size: 2 {{~#if @root.highlightLogic~}}; line-height: 1.51em{{~/if~}}"
   data-library-name="{{ libraryName }}" data-statement-name="{{ statementName }}">
 <code>
 {{> clause}}
