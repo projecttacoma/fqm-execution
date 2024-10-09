b8936ae2325cb87e86425a4e588826adbb9cb7d4
LaurenD <laurend@mitre.org>
2024-10-09 13:38:34 -0400

    Update episode strataCode to have id as backup value

--- src/calculation/MeasureReportBuilder.ts
+++ src/calculation/MeasureReportBuilder.ts
@@ -227,7 +227,7 @@ export default class MeasureReportBuilder<T extends PopulationGroupResult> exten
                 // or s.id (newer measures)
                 const strata: MeasureReportGroupStratifier | undefined =
                   group.stratifier?.find(s => s.code && s.code[0]?.text === stratResults.strataCode) ||
-                  group.stratifier?.find(s => s.id === stratResults.strataId);
+                  group.stratifier?.find(s => s.id === stratResults.strataCode); // strataCode may have an id value if code did not originally exist
                 const stratum = strata?.stratum?.[0];
                 if (stratum) {
                   er.populationResults?.forEach(pr => {
