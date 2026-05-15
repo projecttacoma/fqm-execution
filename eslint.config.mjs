70e1e20887d1f377089f44c1600064c4652e8616
Chris Hossenlopp <hossenlopp@mitre.org>
2026-05-15 11:46:59 -0400

    added regression folders to eslint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,23 @@
 import js from '@eslint/js';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig(
-  globalIgnores(["**/build", "**/connectathon", "**/coverage-percent-script"]),
+  globalIgnores([
+    '**/build',
+    '**/connectathon',
+    '**/coverage-percent-script',
+    '**/regression/default-bundles',
+    '**/regression/bundles'
+  ]),
   js.configs.recommended,
   eslintConfigPrettier,
   tseslint.configs.recommended,
   {
     rules: {
-      "no-case-declarations": "off",
-      "@typescript-eslint/no-explicit-any": "off",
+      'no-case-declarations': 'off',
+      '@typescript-eslint/no-explicit-any': 'off'
     }
   }
 );
